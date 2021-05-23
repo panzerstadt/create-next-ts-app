7aa8570089a775c04cfc30ce217e55d0f4e4875b
panzerstadt <tliqun@gmail.com>
2021-05-23 14:40:06 +0900

    setup ts, fonts

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,41 +4,46 @@ const CardLink = ({ href, title, content }) => {
   return (
     <a
       href={href}
-      className="m-4 p-6 text-left no-underline border border-gray-100 rounded-md hover:text-blue-500 hover:border-blue-500 transition-colors ease-in-out"
+      className="p-6 m-4 text-left no-underline transition-colors ease-in-out border border-gray-100 rounded-md hover:text-blue-500 hover:border-blue-500"
     >
-      <h3 className="mb-4 text-2xl">{title}</h3>
-      <p className="m-0 text-xl">{content}</p>
+      <h3 className="mb-4 font-serif text-2xl font-bold">{title}</h3>
+      <p className="m-0 text-xl font-light">{content}</p>
     </a>
   );
 };
 
 export default function Home() {
   return (
-    <div className="sm:h-screen py-10 sm:py-0 px-1 flex flex-col justify-center items-center ">
+    <div className="flex flex-col items-center justify-center px-1 py-10 sm:h-screen sm:py-0 ">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Serif:wght@400;700&family=Poppins:wght@200;300;500;700;800&display=swap"
+          rel="stylesheet"
+        />
       </Head>
 
-      <main className="py-20 flex flex-col justify-center items-center">
-        <h1 className="sm:text-6xl text-4xl m-0 sm:leading-relaxed leading-loose text-center">
+      <main className="flex flex-col items-center justify-center py-20">
+        <h1 className="m-0 text-4xl leading-loose text-center sm:text-6xl sm:leading-relaxed">
           Welcome to{" "}
           <a
-            className="text-blue-600 no-underline hover:underline focus:underline active:underline text-center"
+            className="text-center text-blue-600 no-underline hover:underline focus:underline active:underline"
             href="https://nextjs.org"
           >
             Next.js!
           </a>
         </h1>
 
-        <p className="sm:text-2xl text-center">
+        <p className="text-center sm:text-2xl">
           Get started by editing{" "}
-          <code className="bg-gray-100 rounded-sm p-2 text-md font-mono">
+          <code className="p-2 font-mono bg-gray-100 rounded-sm text-md">
             pages/index.js
           </code>
         </p>
 
-        <div className="grid mt-10 sm:grid-cols-2 max-w-3xl">
+        <div className="grid max-w-3xl mt-10 sm:grid-cols-2">
           <CardLink
             href="https://nextjs.org/docs"
             title="Documentation &rarr;"
@@ -65,12 +70,12 @@ export default function Home() {
         </div>
       </main>
 
-      <footer className="w-full h-full border-t-2 border-gray-100 flex flex-col justify-center items-center flex-1 pt-6">
+      <footer className="flex flex-col items-center justify-center flex-1 w-full h-full pt-6 border-t-2 border-gray-100">
         <a
           href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
           target="_blank"
           rel="noopener noreferrer"
-          className="flex flex-col justify-center items-center"
+          className="flex flex-col items-center justify-center"
         >
           <p>Powered by</p>
           <img src="/vercel.svg" alt="Vercel Logo" className="h-4 mt-1" />
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,9 +3,13 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    fontFamily: {
+      sans: "Poppins, ui-sans-serif, sans-serif",
+      serif: "PT Serif, serif",
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
